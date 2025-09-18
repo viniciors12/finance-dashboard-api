d90e148e1dcc9b739d26b4158fccd2d56f75f435
viniciors12 <viniciorodriguez97@gmail.com>
2025-09-17 21:57:59 -0600

    updating

---
+++ finance-dashboard-api/Lambda/LambdaEntryPoint.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+using FinanceDashboardApi;
+
+    namespace finance_dashboard_api.Lambda
+    {
+        public class LambdaEntryPoint : APIGatewayProxyFunction
+        {
+            protected override void Init(IWebHostBuilder builder)
+            {
+                builder
+                    .UseStartup<Startup>();
+            }
+        }
+    }
